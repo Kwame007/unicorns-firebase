b090ae55a20d8f365ff9a10c3be43726bc3c7837
Kwame007 <clementnyankuadjei@gmail.com>
2022-08-17 22:11:02 +0000

    add index.js files to folders

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUPTH5iJhyT3HJOe_cn7XtXiFpDF9UEHI",
+  authDomain: "unicorns-firebase.firebaseapp.com",
+  projectId: "unicorns-firebase",
+  storageBucket: "unicorns-firebase.appspot.com",
+  messagingSenderId: "1081100506962",
+  appId: "1:1081100506962:web:7fa0fa4107b99109d5cc8b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
